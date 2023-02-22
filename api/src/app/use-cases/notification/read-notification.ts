8630a5f024a7d721c45d0b2d8cae16729bb8d4b4
Lucas Suares <hub_suares.01@hotmail.com>
2023-02-22 19:25:25 -0300

    feat(api): implement read notification use case

---
+++ api/src/app/use-cases/notification/read-notification.ts
@@ -0,0 +1,29 @@
+import { NotFoundError } from '@app/errors/not-found';
+import { NotificationsRepository } from '@app/repositories/notifications-repository';
+import { Injectable } from '@nestjs/common';
+import { UseCase } from '../use-case';
+
+interface Request {
+  notificationId: string;
+}
+
+type Response = void;
+
+@Injectable()
+export class ReadNotification implements UseCase<Request, Response> {
+  constructor(private notificationsRepository: NotificationsRepository) {}
+
+  async execute(request: Request): Promise<Response> {
+    const { notificationId } = request;
+
+    const notification = await this.notificationsRepository.findById(
+      notificationId,
+    );
+
+    if (!notification) throw new NotFoundError('notification not found');
+
+    notification.read();
+
+    await this.notificationsRepository.save(notification);
+  }
+}
