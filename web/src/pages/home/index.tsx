f2f5ff61d8d7b3acfd63c44cc511b213a02edee5
Lucas Suares <hub_suares.01@hotmail.com>
2023-02-25 19:32:51 -0300

    feat(web): test home page

---
+++ web/src/pages/home/index.tsx
@@ -0,0 +1,10 @@
+import { Box } from "@mui/material";
+import Link from "../../components/link";
+
+export default function Home() {
+  return (
+    <Box>
+      <Link to="/login">Login</Link>
+    </Box>
+  );
+}
